748328f23bebd370a6a7397536784a9778e91429
Matrixmonet <successismyne@gmail.com>
2020-03-06 22:50:07 -0500

    Initial Commit

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -1,12 +1,89 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import unittest
+import subprocess
 import echo
 
-# Your test case class goes here
-class TestEcho(unittest.TestCase):
-    pass
 
-if __name__ == '__main__':
+class my_tests(unittest.TestCase):
+
+    def setUp(self):
+        """let us create one parset to rule them all"""
+        self.parser = echo.create_parser()
+
+    def test_help(self):
+        """ Running the program without arguments should show usage. """
+
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+
+        self.assertEquals(stdout, usage)
+
+    def test_text(self):
+        """testing text parsing of the parser"""
+        args = ["hello world"]
+        namespace = self.parser.parse_args(args)
+        self.assertEqual(namespace.text, "hello world")
+
+    def test_no_args(self):
+        process = subprocess.Popen(
+            ["python", "./echo.py"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage_reminder = "usage: echo.py [-h] [-u] [-l] [-t] text\n"
+        self.assertEqual(stdout, usage_reminder)
+
+    """ testing if shorthand optional flags do the right thing"""
+
+    def test_upper_short(self):
+        args = ["-u", "hello world"]
+        namespace = self.parser.parse_args(args)
+        self.assertEqual(namespace.upper, True)
+        self.assertEqual(echo.main(args), "HELLO WORLD")
+
+    def test_lower_short(self):
+        args = ["-l", "HELLO WORLD"]
+        namespace = self.parser.parse_args(args)
+        self.assertEqual(namespace.lower, True)
+        self.assertEqual(echo.main(args), "hello world")
+
+    def test_title_short(self):
+        args = ["-t", "hello world"]
+        namespace = self.parser.parse_args(args)
+        self.assertEqual(namespace.title, True)
+        self.assertEqual(echo.main(args), "Hello World")
+
+    """ testing if long (--flag) optional flags do the right thing"""
+
+    def test_upper_long(self):
+        args = ["--upper", "hello world"]
+        namespace = self.parser.parse_args(args)
+        self.assertEqual(namespace.upper, True)
+        self.assertEqual(echo.main(args), "HELLO WORLD")
+
+    def test_lower_long(self):
+        args = ["--lower", "HELLO WORLD"]
+        namespace = self.parser.parse_args(args)
+        self.assertEqual(namespace.lower, True)
+        self.assertEqual(echo.main(args), "hello world")
+
+    def test_title_long(self):
+        args = ["--title", "hello world"]
+        namespace = self.parser.parse_args(args)
+        self.assertEqual(namespace.title, True)
+        self.assertEqual(echo.main(args), "Hello World")
+
+    """testing for all args"""
+
+    def test_all(self):
+        args = ["-u", "-l", "-t", 'hello world']
+        namespace = self.parser.parse_args(args)
+        self.assertEqual(namespace.title, True)
+        self.assertEqual(namespace.upper, True)
+        self.assertEqual(namespace.lower, True)
+        self.assertEqual(echo.main(args), "Hello World")
+
+
+if __name__ == "__main__":
     unittest.main()
